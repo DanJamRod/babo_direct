a11d5835742185ea4e41b3e18da5e243f5c69edb
DanJamRod <rodgersd09@gmail.com>
2020-04-22 01:23:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # export FLASK_APP=app.py && python3 -m flask run
 
-from directions.directions import main as directions
+from directions import directions as directions
 from flask import Flask
 
 app = Flask(__name__)
