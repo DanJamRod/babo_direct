0edfb90377130aa4548ccacdb7a56790a3fe1c31
DanJamRod <rodgersd09@gmail.com>
2020-04-24 16:35:33 +0100

    Update directions.py

--- directions.py
+++ directions.py
@@ -1,12 +1,16 @@
 from dijsktra import route as route
+from locations_name import locations_dict as locations_name
 
 def directions(start, finish):
-    return route(start, finish)
-
-def main():
-    """ use for testing
-    """
-    print(directions("beec", "coleman"))
-
-if __name__ == '__main__':
-    main()
+    name = locations_name()
+    directions = []
+    path = route(start, finish)
+    directions.append(f"Leave {name[start]}")
+    for i in range(len(path)-3):
+        if path[i+1][:4] == "path" and name[path[i+1]] != name[path[i+2]]:
+            directions.append(f"Walk along {name[path[i+1]]}")
+        elif path[i+1][:4] != "path": 
+            directions.append(f"Walk past {name[path[i+1]]}")
+    directions.append(f"You are at {name[finish]}!")
+    directions.append(f"This journey should talk around {path[-1]} minutes")
+    return directions
