a62b3e557f21258ff99d4fe23d805df7ddb25864
DanJamRod <rodgersd09@gmail.com>
2020-04-24 20:56:13 +0100

    Delete test.py

--- old_files_archive/test.py
+++
@@ -1,7 +0,0 @@
-import babo_direct
-
-babo_direct.babo_direct("Reynolds", "Sorenson")
-babo_direct.babo_direct("Trim Lot", "Trim")
-babo_direct.babo_direct('Trim', 'Sorenson')
-babo_direct.babo_direct('the Innovation Center', 'the Globe')
-babo_direct.babo_direct('Sorenson', 'Rogers Pub')
