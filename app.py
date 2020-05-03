2e1327c702b5b42f191fa406f1d18722bcdb1966
DanJamRod <rodgersd09@gmail.com>
2020-05-03 22:57:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# export FLASK_APP=app.py && python3 -m flask run
-
 from directions import directions as babo_direct
 from flask import Flask, render_template, request, redirect, url_for, session
 
@@ -9,6 +7,8 @@ app.secret_key = "SuperSecretKey" # Secret Key required for session["start"] to 
 
 @app.route('/',methods=['POST','GET'])
 def index():
+    """ Index/home page. Take inputs of start/end location, and then directs to directions.html
+    """
     if request.method =='POST':
         session["start"] = request.form["start"]
         session["finish"] = request.form["finish"]
@@ -18,8 +18,11 @@ def index():
 
 @app.route("/directions")
 def directions():
+    """ Displays directions, uses imported directions function. If any errors, it displays an error page
+    """
     try:
         text = babo_direct(session["start"],session["finish"])
-        return render_template("directions.html", text = text[:-1], time = text[-1], start = session["start"], finish =session["finish"])
+        return render_template("directions.html", \
+        text = text[:-1], time = text[-1], start = session["start"], finish = session["finish"])  # Last item in list is time taken, see directions.py 
     except:
         return render_template('error.html')
