0f745e7b6ea5ffeee683181f421cd109db5cef06
DanJamRod <rodgersd09@gmail.com>
2020-05-03 19:19:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 # export FLASK_APP=app.py && python3 -m flask run
 
-from directions import directions as directions
+from directions import directions as babo_direct
 from locations_selectable import selectable_locations_dict as locations_dict
 from locations_selectable import selectable_locations_list as locations_list
-from flask import Flask
-
-# Test
-# def index():
-#   return 'hello, world'
-
-# # Users need to see locations_dict.values() i.e. locations_list, 
-# but locations_dict.keys() has to be inputted to the algorithm
-
-# locations_dict = locations_dict()
-# locations_list = locations_list()
-# print(locations_dict)
-# print(f"\n\n")
-# print(locations_list)
-
-# Testing algorithm
-print(directions("becc", "coleman"))
-print(f"\n")
-print(directions("woodland_10", "trim"))
-print(f"\n")
-print(directions("forest", "tomasso"))
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+
+app.secret_key = "SuperSecretKey" # Secret Key required for session["place"] to store info
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    if request.method =='POST':
+        session["start"] = request.form["start"]
+        session["finish"] = request.form["finish"]
+        return redirect(url_for("directions"))
+    else:
+        return render_template("index.html")
+
+@app.route("/directions")
+def directions():
+    try:
+        text = babo_direct(session["start"],session["finish"])
+        return render_template("directions.html", text = text)
+    except:
+        return render_template('error.html')
